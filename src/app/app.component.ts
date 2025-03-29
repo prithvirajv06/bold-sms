1342879c97e4fecc2b96341e0bc549e5f687c996
Prithviraj V <prithvirajv@outlook.com>
2025-03-29 15:21:52 +0530

    Added components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/header/header.component";
+import { SideNavbarComponent } from "./shared/side-navbar/side-navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SideNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
