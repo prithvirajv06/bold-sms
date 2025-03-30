b87250053e9121912a7fe735367b63e9a91a5045
Prithviraj V <prithvirajv@outlook.com>
2025-03-30 23:30:38 +0530

    Dashboard.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { SideNavbarComponent } from "./shared/side-navbar/side-navbar.component";
+import { BreadcrumbComponent } from "./shared/breadcrumb/breadcrumb.component";
+import { DashboardComponent } from "./domain/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SideNavbarComponent],
+  imports: [RouterOutlet, HeaderComponent, SideNavbarComponent, BreadcrumbComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/domain/dashboard/dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss',
+})
+export class DashboardComponent implements OnInit {
+  ngOnInit(): void {}
+}
